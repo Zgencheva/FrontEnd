fb6bf3cdce7fcc42f611319c7b39abac1bc98d53
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2023-03-27 15:51:32 +0300

    To

--- ReactJS/VisitACity/client/src/components/plans-section/plan-details/PlanDetails.js
+++ ReactJS/VisitACity/client/src/components/plans-section/plan-details/PlanDetails.js
@@ -49,7 +49,7 @@ export const PlanDetails = () => {
     }
     return (
         <section id="plan">
-            <h1 className='text-center'>Welcome to your plan to {plan.city}!</h1>
+            <h1 className='text-center'>Welcome to your plan TO {plan.city}!</h1>
             <h3>Attractions:</h3>
             <hr />
             {plan.attractions?.length == 0 &&
